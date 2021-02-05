bfae4be0cd1cc301ebf4379270c8603413e9dc3d
Weverson <weversonneri@outlook.com>
2021-02-05 15:09:08 -0300

    Logo Link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4ca6b883737f0fe4216e";
+/******/ 	var hotCurrentHash = "cf39ce6925c3642ffe74";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
